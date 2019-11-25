192827f9f0223d3cb6c87fee1328f84eab839329
Philip London <philip@blockchain.com>
2019-11-25 18:45:13 +0100

    💫 Add react-styled-flexboxgrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Col, Row } from 'react-styled-flexboxgrid';
 import React from 'react';
 import Workplaces from './Workplaces';
 import styled from 'styled-components';
 
-const AppContainer = styled.div`
+const AppContainer = styled(Row)`
   padding: 3em;
 `;
 const Header = styled.header`
@@ -14,13 +15,22 @@ const Header = styled.header`
 const App: React.FC = () => {
   return (
     <AppContainer>
-      <Header>
-        <span role="img" aria-label="surfer" style={{ marginRight: '0.25em' }}>
-          🏄‍♂️
-        </span>{' '}
-        Philip London
-      </Header>
-      <Workplaces />
+      <Col xs={12} md={6}>
+        <Header>
+          <span
+            role="img"
+            aria-label="surfer"
+            style={{ marginRight: '0.25em' }}
+          >
+            🏄‍♂️
+          </span>{' '}
+          Philip London
+        </Header>
+        <Workplaces />
+      </Col>
+      <Col xs={12} md={6}>
+        Tweets
+      </Col>
     </AppContainer>
   );
 };
