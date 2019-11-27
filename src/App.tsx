f6f691e591ead247c9632f7f1ec213cd0a93720b
Philip London <philip@blockchain.com>
2019-11-27 16:27:50 +0100

    💫 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { Col, Row } from 'react-styled-flexboxgrid';
 import { Header } from './Components';
+import DefaultTheme from './Themes/default';
 import GitHub from './GitHub';
-import React from 'react';
+import LightTheme from './Themes/light';
+import React, { useState } from 'react';
 import Twitter from './Twitter';
 import Workplaces from './Workplaces';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
-import theme from './Themes/default';
 
 interface ProviderProps {
-  theme: { black: string; grey2: string };
+  theme: {
+    black: string;
+    grey1: string;
+    grey2: string;
+    grey3: string;
+    grey4: string;
+    grey5: string;
+    white: string;
+  };
 }
+
+const themes = { default: DefaultTheme, light: LightTheme };
 
 const AppContainer = styled(Col)`
   padding: 3rem;
@@ -18,9 +29,6 @@ const AppContainer = styled(Col)`
 const emojiStyle = { color: 'initial', marginRight: '0.25em' };
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    color: ${(props: ProviderProps) => props.theme['grey2']};
-  }
   html, body {
     font-weight: 600;
     background-color: ${(props: ProviderProps) => props.theme['black']};
@@ -31,11 +39,27 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'default'
+  );
+
+  const handleThemeChange = () => {
+    const nextTheme = theme === 'default' ? 'light' : 'default';
+    localStorage.setItem('theme', nextTheme);
+    setTheme(nextTheme);
+  };
+  const getTheme = () => (theme === 'light' ? LightTheme : DefaultTheme);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={getTheme}>
       <AppContainer>
         <Header>
-          <span role="img" aria-label="surfer" style={emojiStyle}>
+          <span
+            role="img"
+            aria-label="surfer"
+            style={emojiStyle}
+            onClick={handleThemeChange}
+          >
             🏄‍♂️
           </span>{' '}
           Philip London
---
+++ src/Themes/light.ts
@@ -0,0 +1,13 @@
+import { lighten, transparentize } from 'polished';
+
+const black = lighten(0.65, '#000');
+
+export default {
+  black: black,
+  grey1: transparentize(0.1, lighten(0.1, black)),
+  grey2: transparentize(0.1, lighten(0.2, black)),
+  grey3: transparentize(0.1, lighten(0.3, black)),
+  grey4: transparentize(0.1, lighten(0.4, black)),
+  grey5: transparentize(0.1, lighten(0.5, black)),
+  white: '#fff'
+};
