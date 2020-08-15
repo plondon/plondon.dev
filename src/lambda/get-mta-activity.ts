2298618633d26eba5d7befe3f507f61c2a614e29
Philip London <philip@blockchain.com>
2020-08-15 13:58:48 +0200

    Add mta data function

---
+++ src/lambda/get-mta-activity.ts
@@ -0,0 +1,12 @@
+import * as Mta from 'mta-gtfs';
+
+exports.handler = () => {
+  var mta = new Mta({
+    key: process.env.REACT_APP_MTA_TOKEN, // only needed for mta.schedule() method
+    feed_id: 1 // optional, default = 1
+  });
+
+  mta.status().then(function(result) {
+    return result;
+  });
+};
